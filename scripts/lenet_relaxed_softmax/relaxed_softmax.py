c728027d8e451aa97d3b3706f1e9ad780624646a
Khoi <jonandthink@gmail.com>
2018-12-06 09:35:03 +0100

    fix

--- scripts/lenet_relaxed_softmax/relaxed_softmax.py
+++ scripts/lenet_relaxed_softmax/relaxed_softmax.py
@@ -9,18 +9,16 @@ class RelaxedSoftmax(Layer):
         super(RelaxedSoftmax, self).__init__(**kwargs)
 
     def build(self, input_shape):
-        # assert isinstance(input_shape, list)
         # Create a trainable weight variable for this layer.
         super(RelaxedSoftmax, self).build(input_shape)  # Be sure to call this at the end
 
     def call(self, x):
-        # assert isinstance(x, list)
-        # logits = x
-        merged_logits = x
+        logits, temperature = x
+        merged_logits = logits
         relaxed_softmax_ouputs = self.activation(merged_logits)
         return relaxed_softmax_ouputs
 
     def compute_output_shape(self, input_shape):
         # assert isinstance(input_shape, list)
-        shape_a = input_shape
+        shape_a, shape_b = input_shape
         return shape_a
