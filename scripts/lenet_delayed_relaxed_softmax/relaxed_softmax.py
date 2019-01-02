a216f32f3f1d5f3b4f946bd685a33124f44a53ba
Khoi <jonandthink@gmail.com>
2019-01-02 21:52:40 +0100

    asd

---
+++ scripts/lenet_delayed_relaxed_softmax/relaxed_softmax.py
@@ -0,0 +1,26 @@
+#DEPRECATED
+from keras import backend as K
+from keras.engine.topology import Layer
+from keras import activations
+from keras.layers import Multiply
+
+class RelaxedSoftmax(Layer):
+
+    def __init__(self, **kwargs):
+        self.activation = activations.get('softmax')
+        super(RelaxedSoftmax, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        # Create a trainable weight variable for this layer.
+        super(RelaxedSoftmax, self).build(input_shape)  # Be sure to call this at the end
+
+    def call(self, x):
+        logits, temperature = x
+        merged_logits = Multiply()([logits, K.repeat_elements(temperature, 10, 1)])
+        relaxed_softmax_ouputs = self.activation(merged_logits)
+        return relaxed_softmax_ouputs
+
+    def compute_output_shape(self, input_shape):
+        # assert isinstance(input_shape, list)
+        shape_a, shape_b = input_shape
+        return shape_a
