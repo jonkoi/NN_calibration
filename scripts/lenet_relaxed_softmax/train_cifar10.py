4295e308fea74976b43ebddbb6fdd9ab055ad18c
Khoi <jonandthink@gmail.com>
2019-01-03 13:44:25 +0100

    asd

--- scripts/lenet_relaxed_softmax/train_cifar10.py
+++ scripts/lenet_relaxed_softmax/train_cifar10.py
@@ -72,7 +72,7 @@ def build_model(n=1, num_classes = 10, addition = False):
         soft_logits = Add()([soft_logits, b])
     else:
         x = Dense(num_classes + 1, activation = None, kernel_initializer='he_normal', kernel_regularizer=l2(weight_decay) )(x)
-        temperature = Lambda(lambda x : x[:,0])(x)
+        temperature = Lambda(lambda x : x[:,0:1])(x)
         temperature = Lambda(lambda x :1./x)(temperature)
         # temperature = Lambda(lambda x : x*x)(temperature)
         temperature = Lambda(lambda x: tf.Print(x, [x], "temperature = "))(temperature)
